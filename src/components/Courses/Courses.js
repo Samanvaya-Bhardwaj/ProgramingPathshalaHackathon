3368dcdceb8fc926640d2ad9c3e887a9c08aca8d
Samanvaya Bhardwaj <samanvayabhardwaj2001@gmail.com>
2023-02-25 11:58:48 +0530

    card image

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,46 +1,35 @@
 import React from 'react'
+import "./Courses.css"
 import { Card, Button } from 'react-bootstrap'
 
 export default function Courses() {
   return (
     <>
     <h1 className='text-center my-5'>Our Courses</h1>
-    <div className='d-flex align-item-center justify-content-around'>
+    <div className='crd d-flex align-item-center justify-content-around'>
       <div>
       <Card className='my-5 mx-5' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img className="img" variant="top" src="/assets/java.jpeg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>Learn Java</Card.Title>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
       </div>
       <div>
       <Card className='my-5 mx-5' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img className="img" variant="top" src="/assets/react.jpeg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>Learn React</Card.Title>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
       </div>
       <div>
       <Card className='my-5 mx-5' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img className="img" variant="top" src="/assets/cpp.jpeg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>Learn C++</Card.Title>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
@@ -48,13 +37,9 @@ export default function Courses() {
 
       <div>
       <Card className='my-5 mx-5' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img className="img" variant="top" src="/assets/js.jpeg" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>Learn Java Script</Card.Title>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
