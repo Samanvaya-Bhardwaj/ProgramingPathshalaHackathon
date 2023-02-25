06204139a16b4d54a89c3f06192e7d86f85254c1
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-02-25 12:30:06 +0530

    Revert "Merge branch 'main' of https://github.com/vijay-kumar-yadav/ProgramingPathshalaHackathon"
    This reverts commit 25eb3068d9c415bdaf4053dfa500a4991e674cc3, reversing
    changes made to c979ea60efbf08df7f5d1b5962d690f52813ac83.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Contact from "./components/Contact/Contact";
 import Container from "./components/Container/Container";
-import Courses from "./components/Courses/Courses";
 import Navbar from "./components/Navbar/Navbar";
 import Compile from "./components/Compiler/Compile";
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <Navbar/>
     <Compile />
     <Container/>
-    <Courses/>
     <Contact/>
      
     </>
