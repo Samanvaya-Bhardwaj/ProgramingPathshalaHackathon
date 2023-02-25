c979ea60efbf08df7f5d1b5962d690f52813ac83
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-02-25 11:57:35 +0530

    LearnED Coding Compiler

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import Contact from "./components/Contact/Contact";
 import Container from "./components/Container/Container";
 import Navbar from "./components/Navbar/Navbar";
-
+import Compile from "./components/Compiler/Compile";
 function App() {
   return (
     <>
     <Navbar/>
+    <Compile />
     <Container/>
     <Contact/>
      
